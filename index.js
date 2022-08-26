8530b1233c7decdf5fe21bd49867642e87fe1b65
dungdanghung <dungprof8.0@gmail.com>
2022-08-26 10:14:40 +0700

    test

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var data = document.querySelectorAll('.item')
 var nuttrang = document.querySelectorAll('.nuttrang div')
 var next = document.querySelector('#next');
 var prev = document.querySelector('#prev');
+var wrapnut = document.querySelector('.wrapnut')
 var nut = [...nuttrang]
 var datas = [...data]
 var active = 0;
 var kt = true;
+var xoa = true;
 var leng = datas.length
+var t;
+
+setTimeout(() => wrapnut.classList.add('active') , 15000);
 
 function load() {
     if(kt)
@@ -15,18 +20,26 @@ function load() {
         kt = false
     }
     else{
-        // datas.map((da) =>{
-        //     if(da.className.includes('hideitem'))
-        //     {
-        //         da.classList.remove('hideitem')
-        //         return da;
-        //     }
-        // })
+        
+        clearTimeout();
+        datas.map((da) =>{
+            if(da.className.includes('hideitem'))
+            {
+                da.classList.remove('hideitem')
+                return da;
+            }
+        })
         datas.map((da) =>{
             if(da.className.includes('active'))
             {
                 da.classList.remove('active')
-                // da.classList.add('hideitem')
+                da.classList.add('hideitem')
+                if(xoa)
+                {
+                    t = setTimeout(() => da.classList.remove('hideitem') , 3000);
+                }else{
+                    da.classList.remove('hideitem')
+                }
                 return da;
             }
         })
@@ -38,7 +51,7 @@ function load() {
         })
     }
     
-    datas[active].classList.add('active');
+    setTimeout(() => datas[active].classList.add('active') , 0);
     nut[active].classList.add('active')
 
 }
@@ -49,11 +62,11 @@ next.addEventListener('click' , handelnext);
 prev.addEventListener('click' , handelprev);
 function handelnext() {
     active = active < leng -1 ? active + 1 : active = 0;
-    console.log(active)
     load();
 }
 function handelprev() {
     active = active > 0 ? active - 1 : active = leng-1;
-    console.log(active)
+    clearTimeout(t)
     load();
 }
+
